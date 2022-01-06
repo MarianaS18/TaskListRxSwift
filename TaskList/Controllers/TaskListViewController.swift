405f09bf826445658b27d16aaa27225695d5fc31
Mariana <mariana@fastmail.net>
2022-01-06 09:42:13 +0100

    start projekt

---
+++ TaskList/Controllers/TaskListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskListViewController.swift
+//  TaskList
+//
+//  Created by Mariana Steblii on 06/01/2022.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+}
